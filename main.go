e672c4b1393ef363a0f7748aa30a01dfc23c25b9
Aziz <aziz.rahimov0001@gmail.com>
2020-01-05 10:27:52 +0300

    New tests

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func cashback(amount int) int {
 
 	const bound  =  3000
 	const percent = 5
-	result :0
+	result := 0
 
 	if amount >= bound {
 		result = amount * percent / 100
--- main_test.go
+++ main_test.go
@@ -5,19 +5,18 @@ import "testing"
 func Test_cashback(t *testing.T) {
 
 	tests := []struct {
-		name string
+		name   string
 		amount int
-		want int
+		want   int
 	}{
 		// TODO: Add test cases.
 		{"Have cashback", 5000, 250},
 		{"No cashback", 1000, 0},
 		{"Cashback on bound", 3000, 150},
-
+	}
+	for _, test := range tests {
+		got := cashback(test.amount)
+		if got != test.want {
+			t.Error("For cashback test:", test.name, "got:", got, "want:", test.want)
 		}
-		for _, test := range tests {
-			got := cashback(test.amount)
-			if got != test.want {
-				t.Error("For cashback test:", test.name, "got:", got, "want:", test.want)
-}
-}
+	}
